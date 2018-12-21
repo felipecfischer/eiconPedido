0a13a22cb3440c8973c5efb6e479d90ae6f93e32
Felipe Fischer <felipecorreiafischer@hotmail.com>
2018-12-21 00:34:22 -0200

    Metodo insert.

--- src/main/java/br/com/eicon/EiconPedidoApplication.java
+++ src/main/java/br/com/eicon/EiconPedidoApplication.java
@@ -1,13 +1,33 @@
 package br.com.eicon;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EiconPedidoApplication {
+import br.com.eicon.domain.Pedido;
+import br.com.eicon.repositories.PedidoRepository;
 
+@SpringBootApplication
+public class EiconPedidoApplication implements CommandLineRunner{
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EiconPedidoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Pedido pedido1 = new Pedido(null, 3, 3, new java.sql.Timestamp(System.currentTimeMillis()), new BigDecimal(300), new BigDecimal(0), 'A');
+		Pedido pedido2 = new Pedido(null, 4, 4, new java.sql.Timestamp(System.currentTimeMillis()), new BigDecimal(300), new BigDecimal(0), 'A');
+		
+		pedidoRepository.saveAll(Arrays.asList(pedido1, pedido2));
 	}
 
 }
--- src/main/java/br/com/eicon/domain/Pedido.java
+++ src/main/java/br/com/eicon/domain/Pedido.java
@@ -17,7 +17,7 @@ public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer ped_n_id;
 	private Integer ped_cli_n_id;
 	private Integer ped_n_num_controle;
